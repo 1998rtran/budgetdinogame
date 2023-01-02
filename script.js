42293a385328d1ed16d1aa76a56a82a8571e737b
Raymond Tran <1998rtran@gmail.com>
2023-01-01 19:13:25 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const character = document.getElementById("character");
+const block = document.getElementById("block");
+const game = document.getElementById("game");
+
+
+
+
+const checkDead = setInterval(function(){
+    const characterTop = parseInt(window.getComputedStyle(character)
+    .getPropertyValue("top"));
+    const blockLeft = parseInt(window.getComputedStyle(block)
+    .getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>0 && characterTop>=130) {
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("You lose!");
+    }
+}, 10)
+
+function jump(){
+    character.classList.add("animate")
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500);
+};
+game.addEventListener("click", jump);
